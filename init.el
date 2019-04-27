8bf6d5c24288bfbbc66711490776e480ad3226e2
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2019-04-27 17:55:32 +0200

    move some emacs settings to xresources

--- init.el
+++ init.el
@@ -11,25 +11,9 @@
  '(custom-safe-themes
    (quote
     ("0e8bac1e87493f6954faf5a62e1356ec9365bd5c33398af3e83cfdf662ad955f" "86704574d397606ee1433af037c46611fb0a2787e8b6fd1d6c96361575be72d2" default)))
- '(font-use-system-font t)
- '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
     (flycheck counsel org-bullets snazzy-theme ido-describe-bindings ido-grid-mode yasnippet-snippets yasnippet xresources-theme which-key ivy evil)))
- '(scroll-bar-mode nil)
  '(size-indication-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Terminus" :foundry "xos4" :slant normal :weight normal :height 104 :width normal)))))
-(which-key-mode)
-
-
 
 (load-theme 'snazzy)
-
-
-
-
