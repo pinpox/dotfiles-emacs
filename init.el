76432a71a9d217339e6312f52a2b4acf2a58aa56
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2019-04-30 09:48:34 +0200

    some keybindings and diff-hl

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
     ("bc75dfb513af404a26260b3420d1f3e4131df752c19ab2984a7c85def9a2917e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "0e8bac1e87493f6954faf5a62e1356ec9365bd5c33398af3e83cfdf662ad955f" "86704574d397606ee1433af037c46611fb0a2787e8b6fd1d6c96361575be72d2" default)))
  '(package-selected-packages
    (quote
-    (spaceline-all-the-icons spaceline magit flycheck counsel org-bullets snazzy-theme ido-describe-bindings ido-grid-mode yasnippet-snippets yasnippet xresources-theme which-key ivy evil)))
+    (diff-hl spaceline-all-the-icons spaceline magit flycheck counsel org-bullets snazzy-theme ido-describe-bindings ido-grid-mode yasnippet-snippets yasnippet xresources-theme which-key ivy evil)))
  '(size-indication-mode t))
 
 (load-theme 'snazzy)
@@ -23,4 +23,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
