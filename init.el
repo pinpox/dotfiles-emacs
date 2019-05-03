c40fa229de42f0cb23358568ab4ff624811b8b2d
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2019-05-03 10:18:43 +0200

    indent some emacs lisp config blocks

--- init.el
+++ init.el
@@ -38,7 +38,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" default))))
+    ("a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" default)))
+ '(package-selected-packages
+   (quote
+    (format-all yasnippet-snippets which-key use-package spaceline snazzy-theme org-caldav org-bullets magit lsp-ui lsp-java gruvbox-theme general flycheck evil-org evil-leader evil-commentary diff-hl dashboard counsel-projectile company-lsp avy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
