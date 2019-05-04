55e6e14633e1f4c651b0c255c373c49c6474e37b
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2019-05-04 14:54:39 +0200

    fix evil-collection interfering with general's keys

--- init.el
+++ init.el
@@ -2,7 +2,17 @@
 ;;; Commentary:
 ;;  Emacs Startup File --- initialization for Emacs
 ;;; Code:
+
+(require 'package)
+
+(setq package-archives '(("org" . "http://orgmode.org/elpa/")
+                         ("melpa" . "http://melpa.org/packages/")
+                         ("melpa-stable" . "http://stable.melpa.org/packages/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")))
+
+(setq package-enable-at-startup nil)
 (package-initialize)
+
 (require 'org)
 (org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
 (setq tramp-default-method "ssh")
@@ -11,6 +21,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(evil-collection-setup-minibuffer t)
  '(package-selected-packages
    (quote
     (org-pdfview pdf-tools latex-preview-pane format-all evil-collection yasnippet-snippets yasnippet-classic-snippets which-key use-package twilight-theme twilight-anti-bright-theme spaceline snazzy-theme projectile org-bullets magit lsp-ui lsp-java lenlen-theme ivy-hydra gruvbox-theme graphviz-dot-mode go-eldoc git-gutter general fzf flycheck evil-org evil-leader evil-commentary diff-hl dashboard cycle-themes counsel-tramp company-quickhelp company-lsp company-go ample-theme)))
