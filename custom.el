76112b581c8297aa925f3acabd09302a55d097ff
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2019-05-13 22:05:00 +0200

    cleanup, custom functoins

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-collection-setup-minibuffer t)
+ '(package-selected-packages
+   (quote
+    (bug-hunter org-jira dokuwiki ivy-yasnippet vimrc-mode yaml-tomato yaml-mode logview ssh-config-mode config-general-mode yasnippet-snippets yasnippet-classic-snippets xml-rpc which-key use-package twilight-theme twilight-anti-bright-theme spaceline snazzy-theme ox-wk org-pdfview org-bullets mediawiki lsp-ui lsp-java lenlen-theme latex-preview-pane ivy-hydra gruvbox-theme graphviz-dot-mode go-eldoc git-gutter general fzf format-all flycheck flx evil-org evil-magit evil-leader evil-commentary evil-collection dokuwiki-mode diff-hl dashboard cycle-themes counsel-tramp counsel-projectile company-quickhelp company-lsp company-go beacon auctex apache-mode ample-theme)))
+ '(tramp-default-method "ssh" t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- lisp/custom-functions.el
+++ lisp/custom-functions.el
@@ -4,36 +4,34 @@
 ;;; Commentary:
 ;;
 ;;; Code:
-   (defun multiply-by-seven (number)
-       "Multiply NUMBER by seven."
-       (* 7 number))
 
 
          ;; Ever open a file in the wrong window?
-         (defun rotate-windows ()
-           "Rotate your windows"
-           (interactive)
-           (cond ((not (> (count-windows)1))
-                  (message "You can't rotate a single window!"))
-                 (t
-                  (setq i 1)
-                  (setq numWindows (count-windows))
-                  (while  (< i numWindows)
-                    (let* (
-                           (w1 (elt (window-list) i))
-                           (w2 (elt (window-list) (+ (% i numWindows) 1)))
 
-                           (b1 (window-buffer w1))
-                           (b2 (window-buffer w2))
+         ;; (defun rotate-windows ()
+         ;;   "Rotate your windows"
+         ;;   (interactive)
+         ;;   (cond ((not (> (count-windows)1))
+         ;;          (message "You can't rotate a single window!"))
+         ;;         (t
+         ;;          (setq i 1)
+         ;;          (setq numWindows (count-windows))
+         ;;          (while  (< i numWindows)
+         ;;            (let* (
+         ;;                   (w1 (elt (window-list) i))
+         ;;                   (w2 (elt (window-list) (+ (% i numWindows) 1)))
 
-                           (s1 (window-start w1))
-                           (s2 (window-start w2))
-                           )
-                      (set-window-buffer w1  b2)
-                      (set-window-buffer w2 b1)
-                      (set-window-start w1 s2)
-                      (set-window-start w2 s1)
-                      (setq i (1+ i)))))))
+         ;;                   (b1 (window-buffer w1))
+         ;;                   (b2 (window-buffer w2))
+
+         ;;                   (s1 (window-start w1))
+         ;;                   (s2 (window-start w2))
+         ;;                   )
+         ;;              (set-window-buffer w1  b2)
+         ;;              (set-window-buffer w2 b1)
+         ;;              (set-window-start w1 s2)
+         ;;              (set-window-start w2 s1)
+         ;;              (setq i (1+ i)))))))
 
          ;; This snippet flips a two-window frame, so that left is right, or up is down. It's sanity preserving if you've got a sliver of OCD.
 
@@ -63,35 +61,6 @@
          ;;           (set-window-buffer (next-window) next-win-buffer)
          ;;           (select-window first-win)
          ;;           (if this-win-2nd (other-window 1))))))
-
-
-
-
-
-         ;; (defun cleanup-buffer-safe ()
-         ;;   "Perform a bunch of safe operations on the whitespace content of a buffer.
-         ;; Does not indent buffer, because it is used for a before-save-hook, and that
-         ;; might be bad."
-         ;;   (interactive)
-         ;;   (untabify (point-min) (point-max))
-         ;;   (delete-trailing-whitespace)
-         ;;   (set-buffer-file-coding-system 'utf-8))
-
-         ;; ;; Various superfluous white-space. Just say no.
-         ;; (add-hook 'before-save-hook 'cleanup-buffer-safe)
-
-         ;; (defun cleanup-buffer ()
-         ;;   "Perform a bunch of operations on the whitespace content of a buffer.
-         ;; Including indent-buffer, which should not be called automatically on save."
-         ;;   (interactive)
-         ;;   (cleanup-buffer-safe)
-         ;;   (indent-region (point-min) (point-max)))
-
-         ;; (global-set-key (kbd "C-c n") 'cleanup-buffer)
-
-         ;; I use these two literally all the time. The first one removes trailing whitespace and replaces all tabs with spaces before save.
-
-         ;; The last one I've got on a key - it also indents the entire buffer.
 
 
 
@@ -127,6 +96,66 @@
          ;; With these settings, dired buffers are also updated. The last
          ;; setting makes sure that you're not alerted every time this happens.
          ;; Which is every time you save something.
+
+
+
+
+
+
+         ;; Annoyed when Emacs opens the window below instead at the side?
+         ;; This snippet toggles between horizontal and vertical layout of two windows.
+         ;; (defun toggle-window-split ()
+         ;;   (interactive)
+         ;;   (if (= (count-windows) 2)
+         ;;       (let* ((this-win-buffer (window-buffer))
+         ;;              (next-win-buffer (window-buffer (next-window)))
+         ;;              (this-win-edges (window-edges (selected-window)))
+         ;;              (next-win-edges (window-edges (next-window)))
+         ;;              (this-win-2nd (not (and (<= (car this-win-edges)
+         ;;                                          (car next-win-edges))
+         ;;                                      (<= (cadr this-win-edges)
+         ;;                                          (cadr next-win-edges)))))
+         ;;              (splitter
+         ;;               (if (= (car this-win-edges)
+         ;;                      (car (window-edges (next-window))))
+         ;;                   'split-window-horizontally
+         ;;                 'split-window-vertically)))
+         ;;         (delete-other-windows)
+         ;;         (let ((first-win (selected-window)))
+         ;;           (funcall splitter)
+         ;;           (if this-win-2nd (other-window 1))
+         ;;           (set-window-buffer (selected-window) this-win-buffer)
+         ;;           (set-window-buffer (next-window) next-win-buffer)
+         ;;           (select-window first-win)
+         ;;           (if this-win-2nd (other-window 1))))))
+
+
+
+
+
+
+
+
+
+
+         ;; C-d on an empty line in the shell terminates the process.
+
+         ;; (defun comint-delchar-or-eof-or-kill-buffer (arg)
+         ;;   (interactive "p")
+         ;;   (if (null (get-buffer-process (current-buffer)))
+         ;;       (kill-buffer)
+         ;;     (comint-delchar-or-maybe-eof arg)))
+
+         ;; (add-hook 'shell-mode-hook
+         ;;           (lambda ()
+         ;;             (define-key shell-mode-map
+         ;;               (kbd "C-d") 'comint-delchar-or-eof-or-kill-buffer)))
+
+         ;; With this snippet, another press of C-d will kill the buffer.
+
+         ;; It's pretty nice, since you then just tap C-d twice to get rid of the shell and go on about your merry way.
+
+
 
 
 
