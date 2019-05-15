50ff9b0e1101ceafe259fb369c8ee75e89ddf55f
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2019-05-15 11:19:36 +0200

    Add rainbow mode, indent everything

--- custom.el
+++ custom.el
@@ -3,10 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" default)))
  '(evil-collection-setup-minibuffer t)
  '(package-selected-packages
    (quote
-    (bug-hunter org-jira dokuwiki ivy-yasnippet vimrc-mode yaml-tomato yaml-mode logview ssh-config-mode config-general-mode yasnippet-snippets yasnippet-classic-snippets xml-rpc which-key use-package twilight-theme twilight-anti-bright-theme spaceline snazzy-theme ox-wk org-pdfview org-bullets mediawiki lsp-ui lsp-java lenlen-theme latex-preview-pane ivy-hydra gruvbox-theme graphviz-dot-mode go-eldoc git-gutter general fzf format-all flycheck flx evil-org evil-magit evil-leader evil-commentary evil-collection dokuwiki-mode diff-hl dashboard cycle-themes counsel-tramp counsel-projectile company-quickhelp company-lsp company-go beacon auctex apache-mode ample-theme)))
+    (go-mode dracula-theme doom-themes bug-hunter org-jira dokuwiki ivy-yasnippet vimrc-mode yaml-tomato yaml-mode logview ssh-config-mode config-general-mode yasnippet-snippets yasnippet-classic-snippets xml-rpc which-key use-package twilight-theme twilight-anti-bright-theme spaceline snazzy-theme ox-wk org-pdfview org-bullets mediawiki lsp-ui lsp-java lenlen-theme latex-preview-pane ivy-hydra gruvbox-theme graphviz-dot-mode go-eldoc git-gutter general fzf format-all flycheck flx evil-org evil-magit evil-leader evil-commentary evil-collection dokuwiki-mode diff-hl dashboard cycle-themes counsel-tramp counsel-projectile company-quickhelp company-lsp company-go beacon auctex apache-mode ample-theme)))
  '(tramp-default-method "ssh" t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
