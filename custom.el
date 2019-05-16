2da867d5da80362a9bc630b17920cf18a5d2a185
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2019-05-16 08:45:39 +0200

    cleanup comments

--- custom.el
+++ custom.el
@@ -9,8 +9,8 @@
  '(evil-collection-setup-minibuffer t)
  '(package-selected-packages
    (quote
-    (go-mode dracula-theme doom-themes bug-hunter org-jira dokuwiki ivy-yasnippet vimrc-mode yaml-tomato yaml-mode logview ssh-config-mode config-general-mode yasnippet-snippets yasnippet-classic-snippets xml-rpc which-key use-package twilight-theme twilight-anti-bright-theme spaceline snazzy-theme ox-wk org-pdfview org-bullets mediawiki lsp-ui lsp-java lenlen-theme latex-preview-pane ivy-hydra gruvbox-theme graphviz-dot-mode go-eldoc git-gutter general fzf format-all flycheck flx evil-org evil-magit evil-leader evil-commentary evil-collection dokuwiki-mode diff-hl dashboard cycle-themes counsel-tramp counsel-projectile company-quickhelp company-lsp company-go beacon auctex apache-mode ample-theme)))
- '(tramp-default-method "ssh" t))
+    (htmlize ox-reveal go-mode dracula-theme doom-themes bug-hunter org-jira dokuwiki ivy-yasnippet vimrc-mode yaml-tomato yaml-mode logview ssh-config-mode config-general-mode yasnippet-snippets yasnippet-classic-snippets xml-rpc which-key use-package twilight-theme twilight-anti-bright-theme spaceline snazzy-theme ox-wk org-pdfview org-bullets mediawiki lsp-ui lsp-java lenlen-theme latex-preview-pane ivy-hydra gruvbox-theme graphviz-dot-mode go-eldoc git-gutter general fzf format-all flycheck flx evil-org evil-magit evil-leader evil-commentary evil-collection dokuwiki-mode diff-hl dashboard cycle-themes counsel-tramp counsel-projectile company-quickhelp company-lsp company-go beacon auctex apache-mode ample-theme)))
+ '(tramp-default-method "ssh"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -16,16 +16,8 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
- ;; (load "~/.emacs.d/lisp/test.el")
- ;;; Emacs Load Path
-;; (setq load-path (cons "~/.emacs.d/lisp" load-path))
- ;; (load-directory "~/.emacs.d/lisp/")
-;; (let ((default-directory  "~/.emacs.d/lisp/"))
-
 (add-to-list 'load-path (concat user-emacs-directory "lisp"))
-
 (require 'custom-functions)
-;;   (normal-top-level-add-subdirs-to-load-path))
 
 (require 'org)
 (org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
